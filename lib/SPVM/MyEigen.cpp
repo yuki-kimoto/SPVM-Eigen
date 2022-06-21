7078ef663e92c2a4b0d8d99142afc83ffa7dfae8
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-06-21 14:24:36 +0900

    Catch up with SPVM 0.9610

--- lib/SPVM/MyEigen.cpp
+++ lib/SPVM/MyEigen.cpp
@@ -14,10 +14,10 @@ int32_t SPVM__MyEigen__add3d(SPVM_ENV* env, SPVM_VALUE* stack) {
   (void)env;
   
   void* obj_nums1 = stack[0].oval;
-  double* nums1 = env->get_elems_double(env, obj_nums1);
+  double* nums1 = env->get_elems_double(env, stack, obj_nums1);
 
   void* obj_nums2 = stack[1].oval;
-  double* nums2 = env->get_elems_double(env, obj_nums2);
+  double* nums2 = env->get_elems_double(env, stack, obj_nums2);
   
   MatrixXd X1 = Map<MatrixXd>(nums1, 3, 3);
 
@@ -25,8 +25,8 @@ int32_t SPVM__MyEigen__add3d(SPVM_ENV* env, SPVM_VALUE* stack) {
   
   MatrixXd X3 = X1 + X2;
   
-  void* obj_nums3 = env->new_double_array(env, 9);
-  double* nums3 = env->get_elems_double(env, obj_nums3);
+  void* obj_nums3 = env->new_double_array(env, stack, 9);
+  double* nums3 = env->get_elems_double(env, stack, obj_nums3);
   memcpy(nums3, X3.data(), sizeof(double) * 9);
   
   stack[0].oval = obj_nums3;
@@ -37,7 +37,7 @@ int32_t SPVM__MyEigen__add3d(SPVM_ENV* env, SPVM_VALUE* stack) {
 int32_t SPVM__MyEigen__compile_test(SPVM_ENV* env, SPVM_VALUE* stack) {
   (void)env;
   
-  env->die(env, "Error %d", 1);
+  env->die(env, stack, "Error %d", 1);
   
   return 0;
 }
