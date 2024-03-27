9cbffa6c0e97ce61996a4fd86850149be406ebb2
Yuki Kimoto <kimoto.yuki@gmail.com>
2024-03-28 08:44:44 +0900

    Fix MyEigen.

--- lib/SPVM/MyEigen.cpp
+++ lib/SPVM/MyEigen.cpp
@@ -10,23 +10,24 @@ using namespace std;
 
 extern "C" {
   
+static const char* FILE_NAME = "MyEigen.c";
+
 int32_t SPVM__MyEigen__add3d(SPVM_ENV* env, SPVM_VALUE* stack) {
-  (void)env;
   
   void* obj_nums1 = stack[0].oval;
-  double* nums1 = env->get_elems_double(env, obj_nums1);
-
+  double* nums1 = env->get_elems_double(env, stack, obj_nums1);
+  
   void* obj_nums2 = stack[1].oval;
-  double* nums2 = env->get_elems_double(env, obj_nums2);
+  double* nums2 = env->get_elems_double(env, stack, obj_nums2);
   
   MatrixXd X1 = Map<MatrixXd>(nums1, 3, 3);
-
+  
   MatrixXd X2 = Map<MatrixXd>(nums2, 3, 3);
   
   MatrixXd X3 = X1 + X2;
   
-  void* obj_nums3 = env->new_double_array(env, 9);
-  double* nums3 = env->get_elems_double(env, obj_nums3);
+  void* obj_nums3 = env->new_double_array(env, stack, 9);
+  double* nums3 = env->get_elems_double(env, stack, obj_nums3);
   memcpy(nums3, X3.data(), sizeof(double) * 9);
   
   stack[0].oval = obj_nums3;
@@ -37,7 +38,7 @@ int32_t SPVM__MyEigen__add3d(SPVM_ENV* env, SPVM_VALUE* stack) {
 int32_t SPVM__MyEigen__compile_test(SPVM_ENV* env, SPVM_VALUE* stack) {
   (void)env;
   
-  env->die(env, "Error %d", 1);
+  return env->die(env, stack, "Error %d", 1, "MyZlib.c", __func__, FILE_NAME, __LINE__);
   
   return 0;
 }
